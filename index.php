75d230130bc16dcc1198b659b094160825afa67f
xiaolongfeiyun <630307054@qq.com>
2017-12-20 17:36:44 +0800

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$host='0.0.0.0';
+$port=9551;
+$serv=new swoole_server($host,$port);
+
+//当连接时启用
+
+$serv->on('connect',function($serv,$fd){
+	echo '已经连接';
+});
+
+
+$serv->on('receive',function($serv,$fd,$from_id,$data){
+	echo "接受到数据";
+	var_dump($data);
+});
+
+
+$serv->on('close',function($serv,$fd){
+
+	echo '连接关闭';
+});
+
+$serv->start();
+
